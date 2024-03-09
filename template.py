7e9320563339740fd0c7b8fa2cc3384fc7013e7d
Aftab Uddin <aftabbyofficial@gmail.com>
2024-03-09 07:27:38 +0600

    Template for each of the problem solution approach

---
+++ template.py
@@ -0,0 +1,34 @@
+
+'''
+project_name = 
+
+problem_statement = 
+
+problem_source = 
+
+problem_explanation = 
+'''
+
+'''
+*including edge cases, create test cases for each of the scenerios below*
+
+example1_input= 
+example1_output= 
+
+example2_input= 
+example2_output= 
+
+examplen_input= 
+examplen_output= 
+
+steps-
+    Implement the solution and test it using example input, check it results the correct output. Fix bugs, if any
+    Analyze trhe algorithms complexity and identity inefficiencies, if any
+    Apply the right technique to overcome the inefficiencies, then repeat the steps again
+    State the most correct and efficient solution in plain english
+'''
+
+'''
+Algo
+    - 
+'''
